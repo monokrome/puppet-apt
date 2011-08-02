e40c088c25fba70f4edf3ffd4565f365ff1e8330
Brandon R. Stoner <bstoner@yrsi.com>
2011-08-02 21:42:46 +0000

    Modified repository addition to verify that the repository hasn't already been added.

--- manifests/definitions/add_repository.pp
+++ manifests/definitions/add_repository.pp
@@ -1,8 +1,12 @@
 define apt::add::repository ($identifier) {
+	$identifier_file_format = regsubst(regsubst("${identifier}", "ppa:", ""), '\W', "-", "G")
+	$created_filename = "/etc/apt/sources.list.d/${identifier_file_format}-${lsbdistcodename}.list"
+
 	exec {
 		"apt::add::repository::${name}":
 			command => "apt-add-repository ${identifier}",
 			notify => Exec["apt-update"],
+			creates => "${created_filename}",
 			path => ["/sbin", "/bin",
 			         "/usr/sbin", "/usr/bin",
 			         "/usr/local/sbin", "/usr/local/bin"],
